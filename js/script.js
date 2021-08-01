5f08f2edbc7c5e9f861644709e302e30df0e457b
Thalia <thalia.oliv.31@gmail.com>
2021-08-01 17:49:32 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+se os campos email e nome estiverem preenchidos
+ envie os dados
+se nao,
+ peca ao usuario que preencha os campos nome e email 
+
+if(campo de nome estiver preenchido && campo de email estiver preenchido){
+  envie os dados
+}
+
+else{
+  
+}
+
+if(document.getElementById("nome") && document.getElementById("email") {
+
+}else{
+
+}
+document.getElementsByClassName()
