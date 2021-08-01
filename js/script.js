9377ee0149c68c5b6a74a03b965933915324fc77
Thalia <thalia.oliv.31@gmail.com>
2021-08-01 18:27:22 -0300

    removed portuguese instructions

--- js/script.js
+++ js/script.js
@@ -1,15 +1,4 @@
-se os campos email e nome estiverem preenchidos
- envie os dados
-se nao,
- peca ao usuario que preencha os campos nome e email 
 
-if(campo de nome estiver preenchido && campo de email estiver preenchido){
-  envie os dados
-}
-
-else{
-  
-}
 
 if(document.getElementById("nome") && document.getElementById("email") {
 
